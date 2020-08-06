5c34ddbf7e523c68bc0be67583e161199e477836
jalil <celil.ahmad@gmail.com>
2020-08-06 13:35:19 +0400

    -- configuration

---
+++ src/main/java/app/config/ConfigSecurity.java
@@ -0,0 +1,30 @@
+package app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/font/**", "/images/**", "/js/**")
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
+    @Override
+    public void configure(WebSecurity web){
+        web
+                .ignoring()
+                .antMatchers("/resources/**");
+
+    }
+
+}
